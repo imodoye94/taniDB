e9a2a4c2c9a2423fb1fac4cf733a4492ac23f074
IMODOYE AYOKUNBI ABIORO <44514227+imodoye94@users.noreply.github.com>
2023-11-10 03:57:35 +0100

    Create taniIntegerListParser.dart
    initial commit

---
+++ Tani Custom Functions/taniIntegerListParser.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:timeago/timeago.dart' as timeago;
+import '/flutter_flow/lat_lng.dart';
+import '/flutter_flow/place.dart';
+import '/flutter_flow/uploaded_file.dart';
+import '/flutter_flow/custom_functions.dart';
+import '/backend/schema/structs/index.dart';
+
+String? taniIntegerListParser(List<int>? listOfIntegers) {
+  /// MODIFY CODE ONLY BELOW THIS LINE
+
+  // a function that converts an input list of strings to a string, and returns the string
+  if (listOfIntegers == null || listOfIntegers.isEmpty) {
+    return null;
+  }
+  final prereturn = listOfIntegers.join(' |*| ');
+  return '|*[' + prereturn + ']*|';
+
+  /// MODIFY CODE ONLY ABOVE THIS LINE
+}
